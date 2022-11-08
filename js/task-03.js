ba92c2c781d007cd6a5d270539a2bfc5f3e7547f
Ruslan Tendora <ruslantendora@gmail.com>
2022-11-08 11:33:19 +0200

    додає 3тє завдання

--- js/task-03.js
+++ js/task-03.js
@@ -1,37 +1,36 @@
 const images = [
   {
-    url: 'https://images.pexels.com/photos/140134/pexels-photo-140134.jpeg?auto=compress&cs=tinysrgb&dpr=2&h=750&w=1260',
-    alt: 'White and Black Long Fur Cat',
+    url: "https://images.pexels.com/photos/140134/pexels-photo-140134.jpeg?auto=compress&cs=tinysrgb&dpr=2&h=750&w=1260",
+    alt: "White and Black Long Fur Cat",
   },
   {
-    url: 'https://images.pexels.com/photos/213399/pexels-photo-213399.jpeg?auto=compress&cs=tinysrgb&dpr=2&h=750&w=1260',
-    alt: 'Orange and White Koi Fish Near Yellow Koi Fish',
+    url: "https://images.pexels.com/photos/213399/pexels-photo-213399.jpeg?auto=compress&cs=tinysrgb&dpr=2&h=750&w=1260",
+    alt: "Orange and White Koi Fish Near Yellow Koi Fish",
   },
   {
-    url: 'https://images.pexels.com/photos/219943/pexels-photo-219943.jpeg?auto=compress&cs=tinysrgb&dpr=2&h=750&w=1260',
-    alt: 'Group of Horses Running',
+    url: "https://images.pexels.com/photos/219943/pexels-photo-219943.jpeg?auto=compress&cs=tinysrgb&dpr=2&h=750&w=1260",
+    alt: "Group of Horses Running",
   },
 ];
 
-const galleryListEl = document.querySelector(".gallery")
+const galleryListEl = document.querySelector(".gallery");
 
-console.log(galleryListEl)
+// const createImage = images.map(image => {
+//     const listItem = document.createElement("li");
+//     const listImage = document.createElement("img");
+//       listImage.src = image.url
+//       listImage.alt = image.alt
+//       listImage.width = 400;
 
-const createImage = images.map(``````````` => {
-  console.log(image.alt)
-    const listItem = document.createElement("li");
-    const listImage = document.createElement("image");
-      listImage.url = image.url
-      listImage.alt = image.alt
-      console.log(listImage)
-  
-      listItem.append(listImage)
-    
-      console.log(listItem)
-      return listItem;
-  })
+//      listItem.append(listImage)
 
-galleryListEl.append(...createImage)
-  console.log(...createImage)
+//       return listItem;
+//   })
 
+// galleryListEl.append(...createImage)
 
+const listEl = images
+  .map(({ url, alt }) => `<li><img src="${url}" alt="${alt}" width="300" </li>`)
+  .join("");
+
+galleryListEl.insertAdjacentHTML("afterbegin", listEl);
