d9f2fdac9b8d41ee3077dd593244f2ade218f83f
Ruslan Tendora <ruslantendora@gmail.com>
2022-11-08 22:30:27 +0200

    Update task-08.js

--- js/task-08.js
+++ js/task-08.js
@@ -1,1 +1,17 @@
+const form = document.querySelector(".login-form");
 
+const userName = {};
+
+form.addEventListener("submit", onFormSubmit);
+
+function onFormSubmit(event) {
+  event.preventDefault();
+  const formElements = event.currentTarget.elements;
+  if (formElements.email.value === "" || formElements.password.value === "") {
+    return alert("Всі поля мають бути заповнені");
+  } else {
+    userName.email = formElements.email.value;
+    userName.password = formElements.password.value;
+  }
+  console.log(userName);
+}
